ebb2e6c83aa7dd4958346b1659ed1ab6dc146c9f
Thomi <perezthomas17@hotmail.com>
2026-05-17 16:59:23 -0300

    Secciones necesarias del sidebar

--- TPO-Seminario-G2/src/components/Sidebar.jsx
+++ TPO-Seminario-G2/src/components/Sidebar.jsx
@@ -13,9 +13,13 @@ const Sidebar = () => {
       <div className="sidebar-menu-label">MENÚ</div>
       <nav className="sidebar-nav">
         <a href="/dashboard" className="sidebar-link active">Dashboard</a>
-        <a href="/courses" className="sidebar-link">Cursos</a>
+        <a href="/courses" className="sidebar-link">Entregas</a>
         <a href="/correction" className="sidebar-link">Corrección asistida</a>
-        <a href="/questions" className="sidebar-link">Preguntas de repaso</a>
+        <a href="/check" className="sidebar-link">Preguntas de repaso</a>
+        <a href="/following" className="sidebar-link">Seguimiento</a>
+        <a href="/metrics" className="sidebar-link">Métricas del curso</a>
+        <a href="/configuration" className="sidebar-link">Configuración</a>
+        <a href="/profiles" className="sidebar-link">Perfil del alumno</a>
       </nav>
     </aside>
   );
