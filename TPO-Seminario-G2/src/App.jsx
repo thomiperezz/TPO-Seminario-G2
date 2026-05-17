4dd63dc9fa49af38a098f601f5cb041c8ad14fda
Thomi <perezthomas17@hotmail.com>
2026-05-17 17:08:01 -0300

    icono de la página

--- TPO-Seminario-G2/src/App.jsx
+++ TPO-Seminario-G2/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
     <>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Login />} />
       </Routes>
     </>
